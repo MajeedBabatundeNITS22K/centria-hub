dbc37da8585b8776ee4f2d21809a3d32c008d8f9
Serhii Didenko <srg.dd0@gmail.com>
2025-02-04 16:37:47 +0200

    fix: fix moc data

---
+++ app/data/news.ts
@@ -0,0 +1,34 @@
+export const news = [
+	{
+		id: 1,
+		title: "News Title",
+		thumbnail: '/thumbnail.jpg',
+		posted_date: "01-01-2025",
+		text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores similique dolore quae deleniti cupiditate pariatur eveniet, libero animi! Dignissimos dolorem non autem ab molestias? Ipsa amet labore optio veritatis pariatur?"
+
+	},
+	{
+		id: 2,
+		title: "News Title",
+		thumbnail: '/thumbnail.jpg',
+		posted_date: "01-01-2025",
+		text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores similique dolore quae deleniti cupiditate pariatur eveniet, libero animi! Dignissimos dolorem non autem ab molestias? Ipsa amet labore optio veritatis pariatur?"
+
+	},
+	{
+		id: 3,
+		title: "News Title",
+		thumbnail: '/thumbnail.jpg',
+		posted_date: "01-01-2025",
+		text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores similique dolore quae deleniti cupiditate pariatur eveniet, libero animi! Dignissimos dolorem non autem ab molestias? Ipsa amet labore optio veritatis pariatur?"
+
+	},
+	{
+		id: 4,
+		title: "News Title",
+		thumbnail: '/thumbnail.jpg',
+		posted_date: "01-01-2025",
+		text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores similique dolore quae deleniti cupiditate pariatur eveniet, libero animi! Dignissimos dolorem non autem ab molestias? Ipsa amet labore optio veritatis pariatur?"
+
+	}
+]
