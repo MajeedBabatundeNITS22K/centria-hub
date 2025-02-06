7126a0507c0781dccedd0d16c3d3f7eef48ebb28
Misato-Seki <116722027+Misato-Seki@users.noreply.github.com>
2025-02-06 14:03:03 +0200

    feat: add active state to navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,76 +1,85 @@
 "use client";
 import {
-	NavigationMenu,
-	NavigationMenuContent,
-	NavigationMenuItem,
-	NavigationMenuLink,
-	NavigationMenuList,
-	NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu"
-import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
-import { House, Newspaper, Calendar, BadgeInfo, Megaphone, Menu } from 'lucide-react';
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import { House, Newspaper, Calendar, BadgeInfo, Megaphone, Menu } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
-	const navigation = [
-		{ name: "Home", href: "/", icon: <House className="mr-2" /> },
-		{ name: "News", href: "/news", icon: <Megaphone className="mr-2" /> },
-		{ name: "Events", href: "/events", icon: <Calendar className="mr-2" /> },
-		{ name: "Articles", href: "/articles", icon: <Newspaper className="mr-2" /> },
-		{ name: "About", href: "/about", icon: <BadgeInfo className="mr-2" /> },
-	];
+  const navigation = [
+    { name: "Home", href: "/", icon: <House className="mr-2" /> },
+    { name: "News", href: "/news", icon: <Megaphone className="mr-2" /> },
+    { name: "Events", href: "/events", icon: <Calendar className="mr-2" /> },
+    { name: "Articles", href: "/articles", icon: <Newspaper className="mr-2" /> },
+    { name: "About", href: "/about", icon: <BadgeInfo className="mr-2" /> },
+  ];
 
-	return (
-	  <div className='relative flex items-center justify-between px-4 py-2 bg-centriaRed'>
-		  {/* Logo */}
-		  <Link href="/" className='flex items-center md:block hidden'>
-			  <p className='text-2xl font-bold text-white'>Centria Hub</p>
-		  </Link>
+  const location = usePathname();
+  const getNavMenuLinkStyle = (href: string) =>
+    `${navigationMenuTriggerStyle()} ${
+      location === href
+        ? "bg-white text-centriaRed"
+        : "!bg-centriaRed text-white hover:!bg-white hover:!text-centriaRed transition"
+    }`;
 
-		  {/* Desktop Menu */}
-		  <NavigationMenu className='hidden md:flex space-x-4'>
-			  <NavigationMenuList>
-				  <NavigationMenuItem>
-					  {navigation.map((item) => (
-						<NavigationMenuLink
-						  key={item.name}
-						  href={item.href}
-						  className={`${navigationMenuTriggerStyle()} !bg-centriaRed text-white hover:!bg-white hover:!text-centriaRed transition`}
-						>
-							{item.icon}
-							{item.name}
-						</NavigationMenuLink>
-					  ))}
-				  </NavigationMenuItem>
-			  </NavigationMenuList>
-		  </NavigationMenu>
+  return (
+    <div className="relative flex items-center justify-between px-4 py-2 bg-centriaRed">
+      {/* Logo */}
+      <Link href="/" className="flex items-center md:block hidden">
+        <p className="text-2xl font-bold text-white">Centria Hub</p>
+      </Link>
 
-		  {/* Mobile Menu */}
-		  <NavigationMenu className="md:hidden w-full">
-			  <NavigationMenuList>
-				  <NavigationMenuItem>
-					  {/* Humberger */}
-					  <NavigationMenuTrigger className="bg-centriaRed text-white hover:bg-white hover:text-centriaRed transition">
-						  <Menu className=""/>
-					  </NavigationMenuTrigger>
-					  {/* Mobile Menu Content */}
-					  <NavigationMenuContent className="bg-centriaRed">
-						  {navigation.map((item) => (
-							<NavigationMenuLink
-							  key={item.name}
-							  href={item.href}
-							  className={`${navigationMenuTriggerStyle()} !bg-centriaRed text-white hover:!bg-white hover:!text-centriaRed transition !w-full`}
-							>
-								{item.icon}
-								{item.name}
-							</NavigationMenuLink>
-						  ))}
-					  </NavigationMenuContent>
-				  </NavigationMenuItem>
-			  </NavigationMenuList>
-		  </NavigationMenu>
-	  </div>
-	);
+      {/* Desktop Menu */}
+      <NavigationMenu className="hidden md:flex space-x-4">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            {navigation.map((item) => {
+              const location = usePathname();
+              const isActive = location === item.href;
+              return (
+                <NavigationMenuLink key={item.name} href={item.href} className={getNavMenuLinkStyle(item.href)}>
+                  {item.icon}
+                  {item.name}
+                </NavigationMenuLink>
+              );
+            })}
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      {/* Mobile Menu */}
+      <NavigationMenu className="md:hidden w-full">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            {/* Humberger */}
+            <NavigationMenuTrigger className="bg-centriaRed text-white hover:bg-white hover:text-centriaRed transition">
+              <Menu className="" />
+            </NavigationMenuTrigger>
+            {/* Mobile Menu Content */}
+            <NavigationMenuContent className="bg-centriaRed">
+              {navigation.map((item) => (
+                <NavigationMenuLink
+                  key={item.name}
+                  href={item.href}
+                  className={`${getNavMenuLinkStyle(item.href)} !w-full !rounded-none !justify-start`}
+                >
+                  {item.icon}
+                  {item.name}
+                </NavigationMenuLink>
+              ))}
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
 };
 
-export default NavBar
+export default NavBar;
