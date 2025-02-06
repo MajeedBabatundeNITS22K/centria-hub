0b75e35d204f0106f48a7ed3c3152be39116199a
Serhii Didenko <srg.dd0@gmail.com>
2025-02-06 19:35:27 +0200

    config: add formating tools

--- app/(root)/articles/page.tsx
+++ app/(root)/articles/page.tsx
@@ -1,7 +1,7 @@
-export default function ArticlesPage () {
+export default function ArticlesPage() {
 	return (
-	  <div>
-		  <p className="h-[100vh]">This is Articles Page.</p>
-	  </div>
+		<div>
+			<p className='h-[100vh]'>This is Articles Page.</p>
+		</div>
 	)
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,70 +1,67 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+
+import './globals.css'
 
 const nunito = localFont({
-    src: [
-        {
-            path: "./fonts/Nunito-Black.ttf",
-            weight: "900",
-            style: "normal",
-        },
-        {
-            path: "./fonts/Nunito-ExtraBold.ttf",
-            weight: "800",
-            style: "normal",
-        },
-        {
-            path: "./fonts/Nunito-Bold.ttf",
-            weight: "700",
-            style: "normal",
-        },
-        {
-            path: "./fonts/Nunito-SemiBold.ttf",
-            weight: "600",
-            style: "normal",
-        },
-        {
-            path: "./fonts/Nunito-Medium.ttf",
-            weight: "500",
-            style: "normal",
-        },
-        {
-            path: "./fonts/Nunito-Regular.ttf",
-            weight: "400",
-            style: "normal",
-        },
-        {
-            path: "./fonts/Nunito-Light.ttf",
-            weight: "200",
-            style: "normal",
-        },
-        {
-            path: "./fonts/Nunito-ExtraLight.ttf",
-            weight: "100",
-            style: "normal",
-        },
-    ],
-    variable: "--font-nunito-sans",
-});
+	src: [
+		{
+			path: './fonts/Nunito-Black.ttf',
+			weight: '900',
+			style: 'normal',
+		},
+		{
+			path: './fonts/Nunito-ExtraBold.ttf',
+			weight: '800',
+			style: 'normal',
+		},
+		{
+			path: './fonts/Nunito-Bold.ttf',
+			weight: '700',
+			style: 'normal',
+		},
+		{
+			path: './fonts/Nunito-SemiBold.ttf',
+			weight: '600',
+			style: 'normal',
+		},
+		{
+			path: './fonts/Nunito-Medium.ttf',
+			weight: '500',
+			style: 'normal',
+		},
+		{
+			path: './fonts/Nunito-Regular.ttf',
+			weight: '400',
+			style: 'normal',
+		},
+		{
+			path: './fonts/Nunito-Light.ttf',
+			weight: '200',
+			style: 'normal',
+		},
+		{
+			path: './fonts/Nunito-ExtraLight.ttf',
+			weight: '100',
+			style: 'normal',
+		},
+	],
+	variable: '--font-nunito-sans',
+})
 
 export const metadata: Metadata = {
-  title: "Centria Hub",
-  description: "Where Campus Life Converges",
-};
+	title: 'Centria Hub',
+	description: 'Where Campus Life Converges',
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={nunito.variable}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className={nunito.variable}>{children}</body>
+		</html>
+	)
 }
