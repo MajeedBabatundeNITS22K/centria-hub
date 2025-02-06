718de5a73a18efef16efc2ddffcc2672a83cdead
Misato-Seki <116722027+Misato-Seki@users.noreply.github.com>
2025-02-06 15:03:30 +0200

    feat: add back to top button

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import BackToTop from '@/components/BackToTop';
 
 const Layout = ({children}: Readonly<{children: React.ReactNode}>) => {
 	return (
-	  <>
+	  <div className='relative'>
 		  <NavBar/>
 		  {children}
+		  <BackToTop />
 		  <Footer />
-	  </>
+	  </div>
 	)
 }
 export default Layout
