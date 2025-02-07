4ad32efdc1653778a0bf55f24f3204b676e9c276
Misato-Seki <116722027+Misato-Seki@users.noreply.github.com>
2025-02-07 13:45:30 +0200

    chore: edit button style

--- components/BackToTop.tsx
+++ components/BackToTop.tsx
@@ -1,49 +1,59 @@
-"use client";
-import React, { useEffect } from "react";
-import { ArrowUp } from "lucide-react";
-import { useState } from "react";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+'use client'
+
+import { useEffect, useState } from 'react'
+
+import { ArrowUp } from 'lucide-react'
+
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from '@/components/ui/tooltip'
 
 const BackToTop = () => {
-  const [isVisible, setIsVisible] = useState(false);
+	const [isVisible, setIsVisible] = useState(false)
 
-  const handleVisible = () => {
-    if (window.scrollY > 100) {
-      setIsVisible(true);
-    } else {
-      setIsVisible(false);
-    }
-  };
+	const handleVisible = () => {
+		if (window.scrollY > 100) {
+			setIsVisible(true)
+		} else {
+			setIsVisible(false)
+		}
+	}
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleVisible);
-    return () => {
-      window.removeEventListener("scroll", handleVisible);
-    };
-  }, []);
+	useEffect(() => {
+		window.addEventListener('scroll', handleVisible)
+		return () => {
+			window.removeEventListener('scroll', handleVisible)
+		}
+	}, [])
 
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
+	const scrollToTop = () => {
+		window.scrollTo({ top: 0, behavior: 'smooth' })
+	}
 
-  return (
-    isVisible && (
-      <div className="fixed bottom-0 right-0 mr-5 md-5 lg:mb-20">
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger>
-              <div className="rounded-full p-3 bg-gray-500 opacity-80 hover:bg-gray-700" onClick={scrollToTop}>
-                <ArrowUp color="white" size={18}/>
-              </div>
-            </TooltipTrigger>
-            <TooltipContent className="bg-white">
-              <p>Back to Top</p>
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-      </div>
-    )
-  );
-};
+	return (
+		isVisible && (
+			<div className='md-5 fixed bottom-0 right-0 mb-5 mr-5 lg:mb-20'>
+				<TooltipProvider>
+					<Tooltip>
+						<TooltipTrigger>
+							<div
+								className='rounded-full bg-gray-500 p-3 opacity-80 transition-transform duration-300 hover:scale-110 hover:bg-gray-700'
+								onClick={scrollToTop}
+							>
+								<ArrowUp color='white' size={18} />
+							</div>
+						</TooltipTrigger>
+						<TooltipContent className='bg-white'>
+							<p>Back to Top</p>
+						</TooltipContent>
+					</Tooltip>
+				</TooltipProvider>
+			</div>
+		)
+	)
+}
 
-export default BackToTop;
+export default BackToTop
